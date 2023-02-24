649b100707d32885293420625ddfc0e9b5cee4e5
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2023-02-24 11:54:03 -0300

    Feat: implementa a classe BancoTest

--- src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
@@ -1,8 +1,14 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 
 @DisplayName("Testes para a classe Banco")
 class BancoTest {
@@ -10,35 +16,53 @@ class BancoTest {
   @Test
   @DisplayName("1 - Testa o gerador de número único para nova conta.")
   void gerarNumeroNovaContaTest() {
-    fail("Não implementado");
+    Banco banco = new Banco();
+    String novaConta = banco.gerarNumeroNovaConta();
+    assertEquals(10, novaConta.length());
   }
 
   @Test
-  @DisplayName("2 - Testa o método adicionar pessoa cliente retorna o objeto pessoa cliente.")
+  @DisplayName("2 - Testa se o método adicionar pessoa cliente retorna o objeto pessoa cliente.")
   void adicionarPessoaClienteTest() {
-    fail("Não implementado");
-
+    Banco banco = new Banco();
+    assertEquals({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, banco.adicionarPessoaCliente("Adriana", "06992176609", "12345678"));
+    /**
+     * banco.adicionarPessoaCliente("Adriana", "06992176609", "12345678");
+     * ArrayList pessoasClientes = banco.getPessoasClientes();
+     * assertTrue(pessoasClientes.contains({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }));
+     */
   }
 
   @Test
-  @DisplayName("3 - Testa o método login da pessoa cliente retorna o objeto pessoa cliente corretamente.")
+  @DisplayName("3 - Testa se o método login da pessoa cliente retorna o objeto pessoa cliente corretamente.")
   void pessoaClienteLoginTest() {
-    fail("Não implementado");
-
+    Banco banco = new Banco();
+    banco.adicionarPessoaCliente("Adriana", "06992176609", "12345678");
+    assertEquals({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, banco.pessoaClienteLogin("06992176609", "12345678"));
+    assertNull(banco.pessoaClienteLogin("06992176609", "12345679"));
   }
 
   @Test
   @DisplayName("4 - Testa se o método transferir fundos está transferindo corretamente.")
   void depositarTestTransferirFundosTestmostrarExtratoTest() {
-    fail("Não implementado");
-
+    Banco banco = new Banco();
+    banco.gerarNumeroNovaConta();
+    banco.adicionarPessoaCliente("Adriana", "06992176609", "12345678");
+    banco.depositar({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, 0, 10000.00);
+    banco.gerarNumeroNovaConta();
+    banco.transferirFundos({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, 0, 1, 5000.00);
+    assertEquals(5000.00, banco.mostrarExtrato({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, 0));
   }
 
   @Test
   @DisplayName("5 - Testa se o método sacar está funcionando corretamente.")
   void depositarTestSacarTestMostrarExtratoTest() {
-    fail("Não implementado");
-
+    Banco banco = new Banco();
+    banco.gerarNumeroNovaConta();
+    banco.adicionarPessoaCliente("Adriana", "06992176609", "12345678");
+    banco.depositar({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, 0, 10000.00);
+    banco.sacar({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, 0, 5000.00);
+    assertEquals(5000.00, banco.mostrarExtrato({ nome: "Adriana", cpf: "06992176609", senha: "12345678" }, 0));
   }
 
 }
